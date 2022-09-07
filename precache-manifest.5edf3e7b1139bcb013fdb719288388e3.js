22a3482d866444fd33682001653f308818d2190a
Anish Vijay <anishvijay333@gmail.com>
2022-09-07 18:21:05 +0530

    Updates

---
+++ precache-manifest.5edf3e7b1139bcb013fdb719288388e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cba2fc1866f4e1a247f",
+    "url": "/calculx/static/css/main.3545d2a2.chunk.css"
+  },
+  {
+    "revision": "1cba2fc1866f4e1a247f",
+    "url": "/calculx/static/js/main.1cba2fc1.chunk.js"
+  },
+  {
+    "revision": "aa92928c16c4929721f7",
+    "url": "/calculx/static/js/1.aa92928c.chunk.js"
+  },
+  {
+    "revision": "ae89127c7fb3b4e304e1",
+    "url": "/calculx/static/js/runtime~main.ae89127c.js"
+  },
+  {
+    "revision": "a8754dc9efdc8993a6a298e62be34d7a",
+    "url": "/calculx/index.html"
+  }
+];
