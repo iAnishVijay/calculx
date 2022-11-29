5be19c803494703550b3f0106be45ec966af9e49
Anish Vijay <anishvijay333@gmail.com>
2022-11-30 01:17:28 +0530

    hamburger added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import hamburgerIcon from '../images/hamburger-icon.svg';
 
 export default function Header() {
     return (
         <header className="header flex-space-btw">
             <h1 className="header--heading">CalculX</h1>
-                <ul className="header--menus flex-center">
+            <ul className="header--menus flex-center">
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
             </ul>
+            <img className="hamburgerIcon" src={hamburgerIcon} alt="hamburger" />
         </header>
     )
 }
